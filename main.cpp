c67c947c6b2cf85409f026e01ee7c9c2cb678504
Tadashi Ando <ando@experimental.co.jp>
2025-10-26 05:26:32 +0900

    カメラから各ピクセルへの方向の計算を実装。std::numbers::pi_v を使うため C++17 から C++20 に更新。にEigen 5 系に更新。

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
+/*********************************************************************
+ * @file   main.cpp
+ * @brief  レイトレーシングの練習。右手座標系。
+ * @date   2025-10-26
+ *********************************************************************/
+
 #define _SILENCE_CXX17_NEGATORS_DEPRECATION_WARNING
 
-#include "Eigen/Core"
+#include "Eigen/Dense"
 #include <iostream>
 #include <fstream>
 #include <optional>
 #include <vector>
-
-const int IMAGE_WIDTH = 1200; /// 出力する画像の横幅
-const int IMAGE_HEIGHT = 800; /// 出力する画像の高さ
+#include <numbers>
 
 typedef float Real;
 typedef Eigen::Vector3f Vector;
 typedef Vector Color;
+
+constexpr Real operator "" _r( long double x )
+{
+	return static_cast< Real >( x );
+}
+
+constexpr int IMAGE_WIDTH = 1200; /// 出力する画像の横幅
+constexpr int IMAGE_HEIGHT = 800; /// 出力する画像の高さ
+constexpr Real IMAGE_ASPECT = static_cast< Real >( IMAGE_WIDTH ) / static_cast< Real >( IMAGE_HEIGHT ); /// 画像のアスペクト比
+
+constexpr Real degree_to_radian( Real degrre ) { return degrre * std::numbers::pi_v< Real > / 180._r; }
+constexpr Real radian_to_degree( Real radian ) { return radian * 180._r / std::numbers::pi_v< Real >; }
 
 struct Ray
 {
@@ -112,11 +128,6 @@ struct Scene
 	}
 };
 
-Real operator "" _r( long double x )
-{
-	return static_cast< Real >( x );
-}
-
 void save_ppm( const std::string& file_name, Color* image )
 {
 	std::ofstream ofs( file_name );
@@ -148,7 +159,12 @@ int main( int, char** )
 
 	Scene scene;
 
-	Vector camera_position{ 0.f, 0.f, -5.f };
+	Vector camera_position{ 0, 0, 5 };
+	Vector camera_direction = Vector{ 0, 0, -1 }.normalized();
+	Vector camera_up = Vector{ 0, 1, 0 }.normalized();
+	Vector camera_right = camera_direction.cross( camera_up ).normalized();
+	constexpr Real camera_fov = 90._r;
+
 	Vector screen_center{ 0.f, 0.f, 0.f };
 
 	#pragma omp parallel for
@@ -156,11 +172,22 @@ int main( int, char** )
 	{
 		std::cout << "Rendering ( y = " << y << " ) ( " << ( 100.f * y / ( IMAGE_HEIGHT - 1 ) ) << " % )" << std::endl;
 
+		// カメラの中心からスクリーン上の y 座標に対応する位置までの回転角度 ( カメラの X 軸回転 )
+		const Real yr = ( camera_fov / 2._r ) - camera_fov * ( static_cast< Real >( y ) / ( IMAGE_HEIGHT - 1 ) );
+		// std::cout << yr << std::endl;
+
+		const Eigen::AngleAxis< Real > rot_y( degree_to_radian( yr ), camera_right );
+
 		for ( int x = 0; x < IMAGE_WIDTH; x++ )
 		{
-			Vector screen_position{ 0, 0, 0 }; // x, y に対応するスクリーンの位置
+			// カメラの中心からスクリーン上の x 座標に対応する位置までの回転角度 ( カメラの Y 軸回転 )
+			const Real xr = ( camera_fov * IMAGE_ASPECT / 2._r ) - camera_fov * IMAGE_ASPECT * ( static_cast< Real >( x ) / ( IMAGE_WIDTH - 1 ) );
+			// std::cout << xr << std::endl;
 
-			Ray ray{ camera_position, ( screen_position - camera_position ).normalized() };
+			const Eigen::AngleAxis< Real > rot_x( degree_to_radian( xr ), camera_up );
+			// std::cout << d << std::endl;
+
+			Ray ray{ camera_position, rot_x * rot_y * camera_direction };
 
 			auto hit = scene.intersect( ray );
 
@@ -179,4 +206,6 @@ int main( int, char** )
 	}
 
 	save_ppm( "result.ppm", & image[ 0 ] );
+
+	return 0;
 }
